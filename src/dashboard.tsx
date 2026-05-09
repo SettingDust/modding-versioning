fd8633eecbc0f8ef32a4fdd31a42c931d78521e9
SettingDust <settingdust@gmail.com>
2026-05-09 20:01:00 +0800

    style: apply Sentry-inspired design system (dark violet canvas, lime accents, Rubik font)

--- src/dashboard.tsx
+++ src/dashboard.tsx
@@ -118,6 +118,9 @@ function DashboardDocument() {
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>Mod Dependency Dashboard</title>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
+        <link href='https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap' rel='stylesheet' />
         <link rel='stylesheet' href='/app.css' />
       </head>
       <body>
